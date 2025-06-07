61d2d95edaf89b45de77d17291bbe0763c3471c2
Shubhanshu Pandey <webdevshubhanshu@gmail.com>
2025-06-07 14:39:29 +0530

    changed variable names

--- serverfortelephony.js
+++ serverfortelephony.js
@@ -32,8 +32,8 @@ const services = {
     polly: new PollyClient({
         region: "us-east-1",
         credentials: {
-            accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-            secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+            accessKeyId: process.env.accessKeyId,
+            secretAccessKey: process.env.secretAccessKey,
         },
     }),
     openai: new OpenAI({ apiKey: process.env.OPEN_AI })
